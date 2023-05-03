73bb2d8c1e31c4dca97f232259f17f278e65a0ac
JiyauddinSaiyad <zeeyauddinsaiyad@gmail.com>
2023-05-03 13:00:42 +0530

    First Draft

---
+++ src/assets/index.js
@@ -0,0 +1,17 @@
+import linkIcon from './link.svg'
+import loader from './loader.svg'
+import copy from './copy.svg'
+import logo from './logo.svg'
+import tick from './tick.svg'
+import pngLogo from './logo.png'
+import deleteLogo from './delete.svg'
+
+export {
+    linkIcon,
+    loader,
+    copy,
+    logo,
+    tick,
+    pngLogo,
+    deleteLogo
+}
---
+++ src/components/Hero.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+
+import { logo,pngLogo } from "../assets";
+
+const Hero = () => {
+  return (
+    <header className='w-full flex justify-center items-center flex-col'>
+      <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+        <img src={pngLogo} alt='sumz_logo' className='logo w-28 object-contain' />
+
+        <button
+          type='button'
+          onClick={() =>
+            window.open("https://github.com/JiyauddinSaiyad", "_blank")
+          }
+          className='black_btn'
+        >
+           <FontAwesomeIcon icon={faGithub} className='mr-2' />
+          GitHub
+        </button>
+      </nav>
+
+      <h1 className='head_text'>
+        <span >Summarize Articles with </span> <br className='max-md:hidden' />
+        <span className='orange_gradient '>Flash Reads AI by OpenAI GPT-4</span>
+      </h1>
+      <h2 className='desc'>
+        Simplify your reading with Flash Reads AI, an open-source article summarizer
+        that transforms lengthy articles into clear and concise summaries
+      </h2>
+    </header>
+  );
+};
+
+export default Hero;
